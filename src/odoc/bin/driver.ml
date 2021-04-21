2c6a12d18f30640853ca4035ff904000109359c3
Jaap Frolich <jfrolich@gmail.com>
2021-04-21 22:54:23 +0800

    refactoring

--- src/odoc/bin/driver.ml
+++ src/odoc/bin/driver.ml
@@ -1,55 +1,28 @@
+(*
+  TODO:
+    - [ ] store last modified date of unit, and compare to odoc file to see if we need to recompile
+    - [ ] generate odoc and odocl alongside the cmt/cmti files
+    - [ ] don't shell out to the executables, but call directly
+    - [ ] Remove dependency of Bos
+
+
+*)
 let ( let* ) t f = Result.bind t f
 
 let ( let+ ) t f = Result.map f t
 
-(*
-In this section odoc is used to generate the documentation of odoc and some of
-its dependent packages. We can make a few simplifying assumptions here:
+module StringSet = Set.Make (String)
 
-We start with some functions to execute the three phases of odoc.
-
-Compiling a file with odoc requires a few arguments: the file to compile, an
-optional parent, a list of include paths, a list of children for mld files, and
-an output path. Include paths can be just '.', and we can calculate the output
-file from the input because all of the files are going into the same directory.
-
-Linking a file with odoc requires the input file and a list of include paths. As
-for compile we will hard-code the include path.
-
-Generating the HTML requires the input odocl file and an output path. We will
-hard-code the output path to be html.
-
-In all of these we'll ignore stderr.
-*)
 let _ = print_endline "Building docs..."
 
-(*
-   this is the configuration, needs to be picked up from the working directory
-   in a JSON file
-
-   documel.json
-*)
-let dep_libraries =
-  [ "cmdliner"; "astring"; "fpath"; "result"; "yojson"; "tyxml" (* "stdlib" *) ]
-
-let odoc_libraries =
-  [
-    "odoc_xref_test";
-    "print";
-    "odoc_xref2";
-    "odoc_parser";
-    "odoc_odoc";
-    "odoc_model_desc";
-    "odoc_model";
-    "odoc_manpage";
-    "odoc_loader";
-    "odoc_latex";
-    "odoc_html";
-    "odoc_document";
-    "odoc_compat";
-  ]
-
-let root_page = "odoc"
+type config = {
+  (* where to find the units *)
+  unit_root : string;
+  (* the dependencies that documl needs to generate docs for *)
+  dep_libraries : string list;
+  root_page : string;
+  doc_root : string;
+}
 
 let with_error s = function Ok result -> result | Error _ -> raise s
 
@@ -75,6 +48,8 @@ exception CompileDeps
 
 exception BadExtension of string
 
+open Odoc_odoc
+
 let odoc = Bos.Cmd.v "odoc"
 
 let ( % ) = Bos.Cmd.( % )
@@ -95,14 +70,28 @@ let compile file ?parent children =
     | ".cmt" | ".cmti" | ".cmi" -> basename ^ ".odoc"
     | _ -> raise (BadExtension ext)
   in
-  let cmd =
-    odoc % "compile" % Fpath.to_string file % "-I" % "." % "-o" % output_file
+  let directories = [ "." |> Fs.Directory.of_string ] in
+  let _ = Fs.Directory.mkdir_p (Fs.File.dirname file) in
+  let env = Env.create ~important_digests:true ~directories ~open_modules:[] in
+  let result =
+    Compile.compile ~env ~directories
+      ~parent_cli_spec:
+        (match parent with
+        | Some parent -> CliParent parent
+        | None -> CliNoparent)
+      ~hidden:false ~children
+      ~output:(output_file |> Fs.File.of_string)
+      ~warn_error:false file
   in
-  let cmd =
-    List.fold_left (fun cmd child -> cmd % "--child" % child) cmd children
-  in
-  let cmd = match parent with Some p -> cmd % "--parent" % p | None -> cmd in
-  let result = Bos.OS.Cmd.(run_out ~err:err_null cmd |> to_lines) in
+  (* This is the code to shell out to the cmdline *)
+  (* let cmd =
+       odoc % "compile" % Fpath.to_string file % "-I" % "." % "-o" % output_file
+     in
+     let cmd =
+       List.fold_left (fun cmd child -> cmd % "--child" % child) cmd children
+     in
+     let cmd = match parent with Some p -> cmd % "--parent" % p | None -> cmd in
+     let result = Bos.OS.Cmd.(run_out ~err:err_null cmd |> to_lines) in *)
   match result with
   | Ok _ ->
       (* print_endline ("Successfully compiled " ^ Fpath.to_string file); *)
@@ -115,22 +104,50 @@ let compile file ?parent children =
       raise CompileError
 
 let link file =
-  let cmd = odoc % "link" % Bos.Cmd.p file % "-I" % "." in
-  Bos.OS.Cmd.(run_out ~err:err_null cmd |> to_lines) |> with_error LinkError
+  let directories = [ "." |> Fs.Directory.of_string ] in
+  let env = Env.create ~important_digests:true ~directories ~open_modules:[] in
+  Odoc_link.from_odoc ~env ~warn_error:false file
+    Fs.File.(set_ext ".odocl" file)
+  |> with_error LinkError
+(* let cmd = odoc % "link" % Bos.Cmd.p file % "-I" % "." in
+   Bos.OS.Cmd.(run_out ~err:err_null cmd |> to_lines) |> with_error LinkError *)
 
 let html_generate file =
-  let cmd = odoc % "html-generate" % Bos.Cmd.p file % "-o" % "html" in
-  Bos.OS.Cmd.(run_out cmd ~err:err_null |> to_lines)
+  let directories = [ "." |> Fs.Directory.of_string ] in
+  let env = Env.create ~important_digests:false ~directories ~open_modules:[] in
+  Rendering.render_odoc ~env ~renderer:Html_page.renderer ~warn_error:false
+    ~syntax:Odoc_document.Renderer.Reason
+    ~output:(Fs.Directory.of_string "html")
+    {
+      semantic_uris = true;
+      closed_details = false;
+      indent = true;
+      theme_uri = Odoc_html.Tree.Relative None;
+      support_uri = Odoc_html.Tree.Relative None;
+    }
+    file
   |> with_error HtmlGenerationError
+(* let cmd = odoc % "html-generate" % Bos.Cmd.p file % "-o" % "html" in
+   Bos.OS.Cmd.(run_out cmd ~err:err_null |> to_lines)
+   |> with_error HtmlGenerationError *)
 
 let json_generate file =
-  let cmd = odoc % "json-generate" % Bos.Cmd.p file % "-o" % "html" in
-  Bos.OS.Cmd.(run_out cmd ~err:err_null |> to_lines)
+  (* let cmd = odoc % "json-generate" % Bos.Cmd.p file % "-o" % "html" in
+     Bos.OS.Cmd.(run_out cmd ~err:err_null |> to_lines)
+     |> with_error JSONGenerationError *)
+  let directories = [ "." |> Fs.Directory.of_string ] in
+  let env = Env.create ~important_digests:false ~directories ~open_modules:[] in
+  Rendering.render_odoc ~env ~renderer:Odoc_json.Json_renderer.renderer
+    ~warn_error:false ~syntax:Odoc_document.Renderer.Reason
+    ~output:(Fs.Directory.of_string "html")
+    { semantic_uris = true; closed_details = false; indent = true }
+    file
   |> with_error JSONGenerationError
 
 let support_files () =
-  let cmd = odoc % "support-files" % "-o" % "html" in
-  Bos.OS.Cmd.(run_out cmd |> to_lines) |> with_error SupportFilesError
+  Support_files.write ~without_theme:false (Fs.Directory.of_string "html")
+(* let cmd = odoc % "support-files" % "-o" % "html" in
+   Bos.OS.Cmd.(run_out cmd |> to_lines) |> with_error SupportFilesError *)
 
 (*
 We'll now make some library lists. We have not only external dependency
@@ -148,12 +165,10 @@ The following uses ocamlfind to locate the library paths we're looking in for
 our dependencies:
 *)
 
-let ocamlfind = Bos.Cmd.v "ocamlfind"
-
 (* resolving the path for a library *)
 let resolve_lib_path lib =
-  let cmd = Bos.Cmd.(ocamlfind % "query" % lib) in
-  Bos.OS.Cmd.(run_out cmd |> to_lines |> Result.map List.hd)
+  let cmd = Bos.Cmd.v "ocamlfind" % "query" % lib in
+  Bos.OS.Cmd.run_out cmd |> Bos.OS.Cmd.to_lines |> Result.map List.hd
 
 (** constructing (library_name, library_path) tuples *)
 let resolve_lib_paths library_names =
@@ -214,9 +229,9 @@ type compile_deps = { digest : Digest.t; deps : (string * Digest.t) list }
 
 let compile_deps f =
   (* print_endline ("DEPS of " ^ Fpath.to_string f); *)
-  let cmd = Bos.Cmd.(odoc % "compile-deps" % Fpath.to_string f) in
+  let cmd = odoc % "compile-deps" % Fpath.to_string f in
 
-  let* lines = Bos.OS.Cmd.(run_out cmd |> to_lines) in
+  let* lines = Bos.OS.Cmd.run_out cmd |> Bos.OS.Cmd.to_lines in
   let l = List.filter_map (Astring.String.cut ~sep:" ") lines in
   let basename = Fpath.(basename (f |> rem_ext)) |> String.capitalize_ascii in
   match List.partition (fun (n, _) -> basename = n) l with
@@ -231,44 +246,42 @@ library.
 
 type path = Fpath.t
 
-let odoc_all_unit_paths =
-  find_units "../_build/" |> with_error AllUnitPathsError
-
 type unit = Unit_local of Fpath.t | Unit_namespace of string * Fpath.t
 
-let units : (string, unit) Hashtbl.t = Hashtbl.create 10000
-
-let _ =
-  Fpath.Set.iter
-    (fun p ->
-      if not (is_hidden p) then
+let add_local_units ~config units =
+  let odoc_all_unit_paths =
+    find_units config.unit_root |> with_error AllUnitPathsError
+  in
+  let _ =
+    Fpath.Set.iter
+      (fun p ->
         Hashtbl.add units
           (Fpath.basename p |> String.capitalize_ascii)
           (Unit_local p))
-    odoc_all_unit_paths
+      odoc_all_unit_paths
+  in
 
-(* let _ =
-  List.iter (fun (_, path) -> print_endline (Fpath.to_string path)) odoc_units *)
-
-let _ =
-  List.iter
-    (fun (lib, p) ->
-      Fpath.Set.iter
-        (fun p ->
-          if not (is_hidden p) then
+  (* let _ =
+     List.iter (fun (_, path) -> print_endline (Fpath.to_string path)) odoc_units *)
+  let _ =
+    List.iter
+      (fun (lib, p) ->
+        Fpath.Set.iter
+          (fun p ->
             Hashtbl.add units
               (Fpath.basename p |> String.capitalize_ascii)
               (Unit_namespace ("deps", p)))
-        (find_units p |> with_error LibUnitsError))
-    (resolve_lib_paths dep_libraries)
+          (find_units p |> with_error LibUnitsError))
+      (resolve_lib_paths config.dep_libraries)
+  in
 
-let _ =
   Seq.iter
     (fun u ->
       print_endline
         (match u with
         | Unit_local f | Unit_namespace (_, f) -> Fpath.to_string f))
     (Hashtbl.to_seq_values units)
+
 (*
 Let's compile all of the parent mld files. We do this in order such that the
 parents are compiled before the children, so we start with odoc.mld, then
@@ -276,9 +289,9 @@ deps.mld, and so on. The result of this file is a list of the resulting odoc
 files.
 *)
 
-let mkmld x = Fpath.(add_ext "mld" (v x))
-
-let compile_mlds () =
+(* this compiles the mld and markdown files *)
+let compile_docs ~config ~units =
+  let mkmld x = Fpath.(add_ext "mld" (v x)) in
   (* print_endline "Compiling mlds"; *)
   let root_children =
     List.map (fun x -> mkpage x) ("deps" :: extra_docs)
@@ -290,7 +303,7 @@ let compile_mlds () =
       |> List.of_seq)
   in
   List.iter print_endline root_children;
-  let root = compile (mkmld ("" ^ root_page)) root_children in
+  let root = compile (mkmld ("doc/" ^ config.root_page)) root_children in
 
   let deps_children =
     units |> Hashtbl.to_seq_values
@@ -300,33 +313,25 @@ let compile_mlds () =
     |> Seq.map (fun child -> Fpath.basename child |> mkmod)
     |> List.of_seq
   in
-  let deps = compile (mkmld ("" ^ "deps")) ~parent:root_page deps_children in
+  let deps =
+    compile (mkmld ("doc/" ^ "deps")) ~parent:config.root_page deps_children
+  in
 
   let extra_odocs =
-    List.map (fun p -> compile (mkmld ("" ^ p)) ~parent:root_page []) extra_docs
+    List.map
+      (fun p -> compile (mkmld ("doc/" ^ p)) ~parent:config.root_page [])
+      extra_docs
   in
 
   (* print_endline "Done with MLDs"; *)
   List.map Fpath.v (root :: deps :: extra_odocs)
 
-(*
-Now we get to the compilation phase. For each unit, we query its dependencies,
-then recursively call to compile these dependencies. Once this is done we
-compile the unit itself. If the unit has already been compiled we don't do
-anything. Note that we aren't checking the hashes of the dependencies which a
-build system should do to ensure that the module being compiled is the correct
-one. Again we benefit here from the fact that we're creating the docs for one
-leaf package, and that there must be no module name clashes in its dependencies.
-The result of this function is a list of the resulting odoc files.
-*)
-
-module StringSet = Set.Make (String)
-
-let already_compiled = Hashtbl.create 10000
-
-let compile_all () =
+let compile_all ~config =
+  let units : (string, unit) Hashtbl.t = Hashtbl.create 10000 in
+  let already_compiled = Hashtbl.create 10000 in
+  let _ = add_local_units ~config units in
   (* print_endline "Compile all..."; *)
-  let mld_odocs = compile_mlds () in
+  let mld_odocs = compile_docs ~config ~units in
   let rec rec_compile lib file =
     let output = Fpath.(base (set_ext "odoc" file)) in
     (* we need to do this to avoid circular dependencies *)
@@ -342,7 +347,7 @@ let compile_all () =
               | None -> acc
               | Some (Unit_local dep_path) ->
                   let file = find_best_file dep_path in
-                  rec_compile root_page file @ acc
+                  rec_compile config.root_page file @ acc
               | Some (Unit_namespace (lib, dep_path)) ->
                   let file = find_best_file dep_path in
                   rec_compile lib file @ acc)
@@ -362,12 +367,6 @@ let compile_all () =
     (units |> Hashtbl.to_seq_values |> List.of_seq)
   @ mld_odocs
 
-(*
-Linking is now straightforward. We only need to link non-hidden odoc files, as
-any hidden are almost certainly aliased inside the non-hidden ones \(a result
-of namespacing usually, and these aliases will be expanded.
-*)
-
 let link_all odoc_files =
   let not_hidden f = not (is_hidden f) in
   List.map
@@ -375,13 +374,6 @@ let link_all odoc_files =
       ignore (link odoc_file);
       Fpath.set_ext "odocl" odoc_file)
     (List.filter not_hidden odoc_files)
-
-(*
-
-Now we can simply run odoc html-generate over all of the resulting odocl
-files
-
-*)
 
 let generate_all odocl_files =
   List.iter
@@ -393,10 +385,28 @@ let generate_all odocl_files =
   support_files ()
 
 (*
-The following code actually executes all of the above, and we're done!
-*)
+   this is the configuration, needs to be picked up from the working directory
+   in a JSON file
 
-let compiled = compile_all ()
+   documel.json
+*)
+let default_config =
+  {
+    unit_root = "_build/";
+    doc_root = "doc/";
+    root_page = "odoc";
+    dep_libraries =
+      [
+        "cmdliner";
+        "astring";
+        "fpath";
+        "result";
+        "yojson";
+        "tyxml" (* "stdlib" *);
+      ];
+  }
+
+let compiled = compile_all ~config:default_config
 
 let linked = link_all compiled
 
